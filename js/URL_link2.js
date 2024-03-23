280565931fef0101bfad3433660a98f3f0022d32
taehwanlee203 <teein5579@naver.com>
2024-03-23 16:40:38 +0900

    URL token + feedback

--- js/URL_link2.js
+++ js/URL_link2.js
@@ -2,7 +2,7 @@ function addMessageToChatBox(message, type, isHtml = false) {
     const chatBox = document.getElementById('chatBox');
     const messageElement = document.createElement('div');
     messageElement.classList.add('chat-message', type + '-message');
-
+    
     const textElement = document.createElement('div');
     textElement.classList.add('text');
     if (isHtml) {
@@ -10,60 +10,65 @@ function addMessageToChatBox(message, type, isHtml = false) {
     } else {
         textElement.textContent = message; // 텍스트로 처리
     }
-
+    
     messageElement.appendChild(textElement);
     chatBox.appendChild(messageElement);
-}
+};
 
 // 페이지 로딩 시 기본 메시지 추가
-window.onload = function () {
+window.onload = function() {
     addMessageToChatBox('안녕하세요!', 'assistant');
     addMessageToChatBox('댓글을 보고싶은 웹툰의 URL을 입력해주세요.', 'assistant');
 };
-    // 버튼 요소를 찾고, 클릭 이벤트 리스너를 추가합니다.
-    /*document.getElementById('fetchButton').addEventListener('click', function () {
-        const urlInput = document.getElementById('urlInput').value;
-        if (!urlInput) {
-            alert('URL을 입력해주세요.');
-            return;
-        }
-        // 이제 필요한 작업을 수행할 수 있습니다.
-    });*/
+
+document.addEventListener("DOMContentLoaded", function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const token = urlParams.get('token');
+
+    // 토큰을 사용하여 원하는 작업을 수행
+    console.log(token);
+    
+document.getElementById('fetchButton').addEventListener('click', function() {
+    const urlInput = document.getElementById('urlInput').value;
+    if (!urlInput) {
+        alert('URL을 입력해주세요.');
+        return;
+    }
 
     //사용자가 입력한 URL을 사용자의 말풍선으로 표시
     addMessageToChatBox(urlInput, 'user');
 
-    const apiEndpoint = 'https://decalcomanie-yebvymrbqa-du.a.run.app/chat';
+    const token = localStorage.getItem('token'); // 로컬 스토리지에서 토큰 가져오기
+    const apiEndpoint = 'https://decalcomanie-dev-yebvymrbqa-du.a.run.app/chat'; //URL보내주고 OpenAI로 연결 후 답변받는 앤드포인트
     const data = {
         url: urlInput
     };
     fetch(apiEndpoint, {
         method: 'POST',
         headers: {
+            // accessToken값에 로컬스토리지의 token값을 넣고 
             'Content-Type': 'application/json',
+            // 토큰 값 전달 
+            'Authorization': 'Bearer ' + token
         },
         body: JSON.stringify(data)
     })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('네트워크 응답이 올바르지 않습니다.');
-            }
-            return response.json();
-        })
-        // .then(data => {
-        //     console.log(data);
-        //     // API 호출 결과를 사용자에게 표시
-        //     addMessageToChatBox(JSON.stringify(data, null, 2), 'assistant');
-        // })
-        .then(data => {
-            console.log(data);
-            const feedbackWithLineBreaks = data.feedback.replace(/(?<!^)(\d+\.\s)/g, '\n<br>$1');
-            // API 호출 결과 중 "feedback" 키에 해당하는 값만 사용자에게 표시
-            addMessageToChatBox(feedbackWithLineBreaks, 'assistant', true);
-        })
-
-        .catch(error => {
-            console.error('API 호출에 실패했습니다.', error);
-            // 에러 메시지를 사용자에게 표시
-            addMessageToChatBox('API 호출에 실패했습니다: ' + error, 'user');
-        });
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('네트워크 응답이 올바르지 않습니다.');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+        const feedbackWithLineBreaks = data.feedback.replace(/(?<!^)(\d+\.\s)/g, '\n<br>$1');
+        // API 호출 결과 중 "feedback" 키에 해당하는 값만 사용자에게 표시
+        addMessageToChatBox(feedbackWithLineBreaks, 'assistant', true);
+    })
+    .catch(error => {
+        console.error('API 호출에 실패했습니다.', error);
+        // 에러 메시지를 사용자에게 표시
+        addMessageToChatBox('API 호출에 실패했습니다: ' + error, 'user');
+    });
+});
+});
